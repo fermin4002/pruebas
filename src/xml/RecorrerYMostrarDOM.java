a7a706178cff90315746c7b621cb67adede4a88b
Fermin <fermin4002@gmail.com>
2024-09-30 15:58:16 +0200

    primer commit

---
+++ src/xml/RecorrerYMostrarDOM.java
@@ -0,0 +1,37 @@
+package xml;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+
+
+//
+public class RecorrerYMostrarDOM {
+	public static void main(String[]args) {
+		
+	}
+	
+	public Document crearArbol(File fichero) {
+		Document doc=null;
+		try {
+			DocumentBuilderFactory dbf= DocumentBuilderFactory.newInstance();
+			dbf.setIgnoringComments(true);
+			dbf.setIgnoringElementContentWhitespace(true);
+			
+			DocumentBuilder db =dbf.newDocumentBuilder();
+			
+			doc = db.parse(fichero);
+		
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return doc;
+		
+		
+	}
+	
+	
+}//fin main
